0e95b16235d1b75840048fbf2f4083edb92839ab
n2ptune <n2ptune@outlook.com>
2020-02-26 22:05:40 +0900

    add: deploy script

--- postcss.config.js
+++ postcss.config.js
@@ -3,13 +3,8 @@ const tailwindcss = require('tailwindcss')
 const postcssPurgecss = require(`@fullhuman/postcss-purgecss`)
 
 const purgecss = postcssPurgecss({
-  // Specify the paths to all of the template files in your project.
-  content: ['./public/**/*.html', './src/**/*.vue'],
-  // Include any special characters you're using in this regular expression.
-  // See: https://tailwindcss.com/docs/controlling-file-size/#understanding-the-regex
-  defaultExtractor: content => content.match(/[\w-/:](?<!:)/g) || [],
-  // Whitelist auto generated classes for transitions and router links.
-  // From: https://github.com/ky-is/vue-cli-plugin-tailwind
+  content: ['./public/**/*.html', './src/**/*.vue', './src/**/*.js'],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
   whitelistPatterns: [
     /-(leave|enter|appear)(|-(to|from|active))$/,
     /^(?!(|.*?:)cursor-move).-move$/,
@@ -21,6 +16,6 @@ module.exports = {
   plugins: [
     autoprefixer,
     tailwindcss,
-    ...(process.env.NODE_ENV !== 'development' ? [purgecss] : [])
+    process.env.NODE_ENV !== 'development' ? purgecss : ''
   ]
 }
--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,14 @@ module.exports = {
         '@': path.join(__dirname, 'src/'),
         '@@': path.join(__dirname, './')
       }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(s*)css$/,
+          use: ['postcss-loader']
+        }
+      ]
     }
   }
 }
